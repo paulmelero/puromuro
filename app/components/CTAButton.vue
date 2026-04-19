fae2fb51a97e01a528b154648ee5b95e58c8982c
paulmelero <paul.melero@backmarket.com>
2026-04-19 21:27:58 +0200

    Fix CTA

--- app/components/CTAButton.vue
+++ app/components/CTAButton.vue
@@ -5,13 +5,15 @@ const props = defineProps<{
   to?: string;
 }>();
 
+const hasTo = computed(() => !!props.to);
 const Component = computed(() => {
-  return props.to ? NuxtLink : 'button';
+  return hasTo.value ? NuxtLink : 'button';
 });
 </script>
 
 <template>
   <component
+    v-bind="hasTo ? { to: props.to } : {}"
     class="cursor-pointer inline-block bg-brick text-white font-heading text-xs font-bold tracking-[0.2em] uppercase px-8 py-4 hover:bg-orange [--drip-color:var(--color-brick)] hover:[--drip-color:var(--color-orange)] transition-colors no-underline rounded-md"
   >
     <slot />
